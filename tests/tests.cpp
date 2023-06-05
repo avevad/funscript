b7a82a4c601fbf68e5c6310de1a4a99024685973
Vadim Averin <avevad0808@gmail.com>
2023-06-05 21:24:09 +0500

    Add more extensive tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -43,72 +43,106 @@ TEST_CASE("Floating point numbers", "[floats]") {
 
 TEST_CASE("Variables and scopes", "[scopes]") {
     TestEnv env;
-    CHECK_THAT("var = 1", FAILS);
-    REQUIRE_THAT(".var = 1", SUCCEEDS);
-    REQUIRE_THAT("var", EVALUATES_TO(1));
-    CHECK_THAT("(.var2 = 1; var)", EVALUATES_TO(1));
-    CHECK_THAT("var2", FAILS);
-    CHECK_THAT("(.var = 2; var)", EVALUATES_TO(2));
-    CHECK_THAT("var", EVALUATES_TO(1));
-}
-
-TEST_CASE("Functions", "[functions]") {
-    TestEnv env;
-    REQUIRE_THAT(".sum = (.a, .b): a + b", SUCCEEDS);
-    CHECK_THAT("sum(13, 27)", EVALUATES_TO(13 + 27));
-    REQUIRE_THAT(".sum3 = (.a, .b, .c): sum(a, b) + c", SUCCEEDS);
-    CHECK_THAT("sum3(1, 10, 15)", EVALUATES_TO(1 + 10 + 15));
-    CHECK_THAT("sum3(1, 5)", FAILS);
-    CHECK_THAT("sum3()", FAILS);
-    CHECK_THAT("sum3(1, 2, 3, 4)", FAILS);
-    REQUIRE_THAT(".plus_minus = .n: (n - 1, n + 1)", SUCCEEDS);
-    CHECK_THAT("plus_minus 5", EVALUATES_TO(4, 6));
-    CHECK_THAT("plus_minus(5, 6)", FAILS);
-    CHECK_THAT("sum3(1, plus_minus 9)", EVALUATES_TO(1 + 8 + 10));
-    CHECK_THAT("a", FAILS);
-    CHECK_THAT("n", FAILS);
-}
-
-TEST_CASE("Conditionals", "[conditionals]") {
-    TestEnv env;
-    REQUIRE_THAT(".max = (.a, .b): (a > b then a else b)", SUCCEEDS);
-    REQUIRE_THAT(".val1 = 349 * 512", SUCCEEDS);
-    REQUIRE_THAT(".val2 = 237 * 601", SUCCEEDS);
-    CHECK_THAT("max(val1, val2)", EVALUATES_TO(std::max(349 * 512, 237 * 601)));
-    REQUIRE_THAT(".filter = .pred: .val: (pred(val) then val)", SUCCEEDS);
-    REQUIRE_THAT(".not7 = .val: val != 7", SUCCEEDS);
-    CHECK_THAT("filter(not7)(5)", EVALUATES_TO(5));
-    CHECK_THAT("filter(not7)(7)", EVALUATES_TO());
+    SECTION("Declaration") {
+        REQUIRE_THAT("newvar = 1", FAILS);
+        REQUIRE_THAT(".newvar = 1", SUCCEEDS);
+        CHECK_THAT("newvar == 1", EVALUATES_TO(true));
+    };
+    SECTION("Shadowing") {
+        REQUIRE_THAT(".var = 1", SUCCEEDS);
+        CHECK_THAT("(.var = 2; var)", EVALUATES_TO(2));
+        CHECK_THAT("var", EVALUATES_TO(1));
+    };
+    SECTION("Lifetime") {
+        REQUIRE_THAT("(.var = 1)", SUCCEEDS);
+        CHECK_THAT("var", FAILS);
+    };
 }
 
 TEST_CASE("Assignment expressions", "[assignments]") {
     TestEnv env;
-    REQUIRE_THAT(".a, .b = 1, 2", SUCCEEDS);
-    REQUIRE_THAT("a, b", EVALUATES_TO(1, 2));
-    REQUIRE_THAT("a, b = b, a", SUCCEEDS);
-    REQUIRE_THAT("a, b", EVALUATES_TO(2, 1));
-    REQUIRE_THAT(".x, .y, .z = a, b", FAILS);
-    CHECK_THAT("x, y", EVALUATES_TO(2, 1));
-    REQUIRE_THAT("a, b, x = x, a, b, c", FAILS);
+    SECTION("Multiple assignment") {
+        REQUIRE_THAT(".a, .b = 1, 2", SUCCEEDS);
+        CHECK_THAT("a, b", EVALUATES_TO(1, 2));
+    };
+    SECTION("Swapping") {
+        REQUIRE_THAT(".foo, .bar = yes, no", SUCCEEDS);
+        REQUIRE_THAT("foo, bar = bar, foo", SUCCEEDS);
+        CHECK_THAT("foo, bar", EVALUATES_TO(false, true));
+    };
+    SECTION("Underflow") {
+        REQUIRE_THAT(".var1, .var2 = 123, 456", SUCCEEDS);
+        REQUIRE_THAT(".var3, .var4, .var5 = var1, var2", FAILS);
+        CHECK_THAT("var3 == var1", EVALUATES_TO(true));
+        CHECK_THAT("var4 == var2", EVALUATES_TO(true));
+    };
+    SECTION("Overflow") {
+        REQUIRE_THAT(".lorem, .ipsum = 'lorem', 'ipsum', 'dolor', 'sit', 'amet'", FAILS);
+        CHECK_THAT("lorem, ipsum", EVALUATES_TO("lorem", "ipsum"));
+    };
+}
+
+TEST_CASE("Conditionals", "[conditionals]") {
+    TestEnv env;
+    SECTION("Conditional operator") {
+        REQUIRE_THAT(".answer = 42", SUCCEEDS);
+        CHECK_THAT("answer == 32 then 'it cannot be'", EVALUATES_TO());
+        CHECK_THAT("answer == 42 then 'of course it is'", EVALUATES_TO("of course it is"));
+    };
+    SECTION("Else clause") {
+        REQUIRE_THAT(".val1, .val2 = 54, 35", SUCCEEDS);
+        CHECK_THAT("val1 > val2 then val1 else val2", EVALUATES_TO(54));
+        CHECK_THAT("val1 == val2 then 'same' else 'different'", EVALUATES_TO("different"));
+    }
+}
+
+TEST_CASE("Functions", "[functions]") {
+    TestEnv env;
+    SECTION("Creation and calling") {
+        REQUIRE_THAT(".sum = (.a, .b): a + b", SUCCEEDS);
+        CHECK_THAT("sum(13, 27)", EVALUATES_TO(13 + 27));
+        CHECK_THAT("a", FAILS);
+        CHECK_THAT("b", FAILS);
+    };
+    SECTION("Arguments") {
+        REQUIRE_THAT(".sum3 = (.a, .b, .c): a + b + c", SUCCEEDS);
+        CHECK_THAT("sum3(1, 2, 3, 4)", FAILS);
+        CHECK_THAT("sum3(1, 10, 15)", EVALUATES_TO(1 + 10 + 15));
+        CHECK_THAT("sum3(1, 5)", FAILS);
+        CHECK_THAT("sum3()", FAILS);
+    };
+    SECTION("Multiple return") {
+        REQUIRE_THAT(".sum5 = (.a, .b, .c, .d, .e): a + b + c + d + e", SUCCEEDS);
+        REQUIRE_THAT(".plus_minus = .n: (n - 1, n + 1)", SUCCEEDS);
+        CHECK_THAT("plus_minus 5", EVALUATES_TO(4, 6));
+        CHECK_THAT("sum5(plus_minus 2, 5, plus_minus 8)", EVALUATES_TO(1 + 3 + 5 + 7 + 9));
+    };
 }
 
 TEST_CASE("Strings", "[strings]") {
     TestEnv env;
-    REQUIRE_THAT(".str1 = 'amogus'", SUCCEEDS);
-    REQUIRE_THAT(".str2 = 'is sus'", SUCCEEDS);
-    CHECK_THAT("str1 + ' ' + str2", EVALUATES_TO("amogus is sus"));
+    SECTION("Creation") {
+        REQUIRE_THAT(".empty = ''", SUCCEEDS);
+        REQUIRE_THAT(".some = 'some str'", SUCCEEDS);
+        CHECK_THAT("some, empty", EVALUATES_TO("some str", ""));
+    };
+    SECTION("Concatenation") {
+        REQUIRE_THAT(".str1 = 'amogus'", SUCCEEDS);
+        REQUIRE_THAT(".str2 = 'is sus'", SUCCEEDS);
+        CHECK_THAT("str1 + ' ' + str2", EVALUATES_TO("amogus is sus"));
+    };
 }
 
 TEST_CASE("Loops", "[loops]") {
     TestEnv env;
-    SECTION("Precondition loop") {
+    SECTION("Pre-condition loop") {
         REQUIRE_THAT(".i = 0; i != 5 do (i, (i = i + 1))", EVALUATES_TO(0, 1, 2, 3, 4));
         REQUIRE_THAT("(1 == 0) do (5)", EVALUATES_TO());
     };
-    SECTION("Postcondition loop") {
+    SECTION("Post-condition loop") {
         REQUIRE_THAT(".i = 0; (i = i + 1; i) until i == 7", EVALUATES_TO(1, 2, 3, 4, 5, 6, 7));
         REQUIRE_THAT("'some str' until 2 * 2 == 4", EVALUATES_TO("some str"));
-    }
+    };
 }
 
 TEST_CASE("Arrays", "[arrays]") {
@@ -136,5 +170,5 @@ TEST_CASE("Arrays", "[arrays]") {
         REQUIRE_THAT(".abc = ['a', 'b', 'c']", SUCCEEDS);
         REQUIRE_THAT(".defgh = ['d', 'e', 'f', 'g', 'h']", SUCCEEDS);
         REQUIRE_THAT("(abc + defgh)[6, 5, 1]", EVALUATES_TO("g", "f", "b"));
-    }
+    };
 }
